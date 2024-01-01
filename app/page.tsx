df18a5f0c0b099ddbbb5d89485c7903bc31bbf8e
Izaz Ahamed <izazahamedpro2017@gmail.com>
2024-01-01 11:07:02 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link';
+
+ 
 
 export default function Home() {
   return (
 <main>
-  <div className=''>hello world izaz</div>
+  <div className=''>hello world shiek</div>
   <Image
         src="next.svg" // Replace with the actual path to your image
         alt="Description of the image"
         width={100} // Set the width of the image
         height={100} // Set the height of the image
       />
-      <div>oiii</div>
+      <div>oiii paiya</div>
 
       <Image
        src="vercel.svg"  alt="Description of the image" width={100} 
         height={100}/>
-
+ <Link legacyBehavior href="/other-page">
+    <a>Go to other page</a>
+  </Link>
 
        
 </main>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export",
   distDir: "build",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Izazv3.github.io" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
