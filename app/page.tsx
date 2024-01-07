8512a48f2e1d94229661b466d8128e095b7c5245
Izaz Ahamed <izazahamedpro2017@gmail.com>
2024-01-07 18:49:00 +0530

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-import Image from 'next/image'
-import Link from 'next/link';
-
- 
+import React from "react";
 
 export default function Home() {
   return (
-<main>
-  <div className=''>hello world peter maaame</div>
-  <Image
-        src="next.svg" // Replace with the actual path to your image
-        alt="Description of the image"
-        width={100} // Set the width of the image
-        height={100} // Set the height of the image
-      />
-      <div>oiii paiya</div>
-
-      <Image
-       src="vercel.svg"  alt="Description of the image" width={100} 
-        height={100}/>
- <Link legacyBehavior href="/other-page">
-    <a>Go to other page</a>
-  </Link>
-
-       
-</main>
-  )
+    <main>
+      <div className="flex items-center justify-center flex-row">
+        <h1>hello world izaz</h1>
+      </div>
+    </main>
+  );
 }
